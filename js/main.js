d2b0a178ec3fc311666ec7caa450bb5d928be82f
torco34 <torcoromaarias@gimail.com>
2020-12-10 20:35:47 -0500

    arreglo con js, no terminado

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-const header = document.querySelector('header')
-console.log(header)
-// class 
+const header = document.getElementById('header')
+let ubicacion = window.pageYOffset
+
+console.log(ubicacion, header)
+
+window.onscroll = function(){
+let desplaza = window.pageYOffset
+if(ubicacion < desplaza ){
+	// header.style.top = '0'
+	header.style.transition = "0.5s";
+    header.style.top = "-120px"
+	// header.style.background = ""
+	console.log(desplaza)
+
+}else {
+	
+	
+	header.style.top = "0px"
+	header.style.transition = "0.5s";
+	// header.style.background = "#497e6a"
+}
+ubicacion = desplaza
+console.log(desplaza, ubicacion )
+}
