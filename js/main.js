7aa9525fa36d0d8115718462f30e5827d6165116
torco34 <torcoromaarias@gimail.com>
2020-12-11 03:15:57 -0500

    terminado el css header js lista

--- js/main.js
+++ js/main.js
@@ -5,20 +5,18 @@ console.log(ubicacion, header)
 
 window.onscroll = function(){
 let desplaza = window.pageYOffset
+
+
 if(ubicacion < desplaza ){
-	// header.style.top = '0'
-	header.style.transition = "0.5s";
+	header.style.top = '0'
+	header.style.transition = "0.3s";
     header.style.top = "-120px"
-	// header.style.background = ""
 	console.log(desplaza)
 
 }else {
 	
-	
-	header.style.top = "0px"
-	header.style.transition = "0.5s";
-	// header.style.background = "#497e6a"
+	header.style.transition = "0.6s";
+	header.style.top = '0px'
 }
-ubicacion = desplaza
-console.log(desplaza, ubicacion )
+
 }
