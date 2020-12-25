976e63c2852795ece36949c488fe697c2096dc54
torco34 <torcoromaarias@gimail.com>
2020-12-25 13:44:12 -0500

    primer codijo con POO

--- js/obj.js
+++ js/obj.js
@@ -1,18 +1,6 @@
-class NombreLista{
-	constructor(inicio, menu,  sabores, ingresar){
-     this.inicio = inicio
-     this.menu = menu
-     this.sabores =  sabores
-     this.ingresar = ingresar
-	}
-	mostrarTaps(){
-		console.log(`hola, soy : ${this.nombre}, ${this.apellido}`)
-	}
-}
-
-let tasp = new NombreLista(`${this.menu}`)
 
 
+const header = document.getElementById('header')
 const inicio = document.getElementById('inicio')
 const menu = document.getElementById('menu')
 const sabores = document.getElementById('sabor')
@@ -23,10 +11,18 @@ const taps1 = document.getElementById('taps1')
 const taps2 = document.getElementById('taps2')
 const taps3 = document.getElementById('taps3')
 const taps4 = document.getElementById('taps4')
-// function Persona(nombre, apellidos, edad, ){
-// 	this.nombre = nombre
-// 	this.apellidos = apellidos
-// 	this.edad = edad
-// }
+ 
 
-// let toco = new Persona('torco', 'arias', 35)
+ class MostrarPestana{
+ 	constructor(){
+     this.pestana()
+ 	}
+ 	pestana(){
+ 		taps4.style.display = "block"
+ 		taps1.style.display = 'none'
+ 	}
+ }
+header.addEventListener('click', () =>{
+	let mostrarPestana = new MostrarPestana()
+	// alert('hola')
+})
