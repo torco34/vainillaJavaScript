ff5ffcadeef8d1700a778b8f72a4fa0455882053
Torcoroma <torcoromaarias@gmail.com>
2022-05-04 10:30:57 -0500

    Arreglo final

--- js/main.js
+++ js/main.js
@@ -1,108 +1,97 @@
-const header = document.getElementById('header')
-const quitarImg = document.getElementById('foto_header')
-let ubicacion = window.pageYOffset
+const header = document.getElementById("header");
+const quitarImg = document.getElementById("foto_header");
+let ubicacion = window.pageYOffset;
 
-console.log(header)
+console.log(header);
 
-window.onscroll = function(){
-let desplaza = window.pageYOffset
-console.log(header)
+window.onscroll = function () {
+  let desplaza = window.pageYOffset;
+  console.log(header);
 
-if(ubicacion < desplaza ){
-	header.style.top = '0'
-	header.style.transition = "0.6s";
-    header.style.top = '-120px'
-
-
-}else {
-	
-	header.style.transition = "0.6s";
-	header.style.top = '0px'
-}
-
-}
+  if (ubicacion < desplaza) {
+    header.style.top = "0";
+    header.style.transition = "0.6s";
+    header.style.top = "-120px";
+  } else {
+    header.style.transition = "0.6s";
+    header.style.top = "0px";
+  }
+};
 // creando pestanas
-const inicio = document.getElementById('inicio')
-const menu = document.getElementById('menu')
-const sabores = document.getElementById('sabor')
-const ingresar = document.getElementById('ingresar')
+const inicio = document.getElementById("inicio");
+const menu = document.getElementById("menu");
+const sabores = document.getElementById("sabor");
+const ingresar = document.getElementById("ingresar");
 
 // las API del pestana
-const taps1 = document.getElementById('taps1')
-const taps2 = document.getElementById('taps2')
-const taps3 = document.getElementById('taps3')
-const taps4 = document.getElementById('taps4')
-
+const taps1 = document.getElementById("taps1");
+const taps2 = document.getElementById("taps2");
+const taps3 = document.getElementById("taps3");
+const taps4 = document.getElementById("taps4");
+const taps5 = document.getElementById("taps5");
+console.log(taps2);
+console.log(taps1);
+console.log(taps3);
 // const documen = document.getElementById('header')
 // console.log(documen)
-const pesMenu = () =>{
-// documen.classList.toggle("header")
-taps1.style.display = "none"
-taps2.style.display = "block"
-taps3.style.display = "none"
-taps4.style.display = "none"
-// quitarImg.style.display = 'none'
-inicio.classList.remove('active')
-menu.classList.remove('active')
-inicio.classList.remove('active')
-sabores.classList.remove('active')
-ingresar.classList.remove('active')
-menu.classList.add('active')
-
-} 
-
-
-
+const pesMenu = () => {
+  // documen.classList.toggle("header")
+  taps1.style.display = "none";
+  taps2.style.display = "block";
+  taps3.style.display = "none";
+  taps4.style.display = "none";
+  taps5.style.display = "none";
+  // quitarImg.style.display = 'none'
+  inicio.classList.remove("active");
+  menu.classList.remove("active");
+  inicio.classList.remove("active");
+  sabores.classList.remove("active");
+  ingresar.classList.remove("active");
+  menu.classList.add("active");
+};
 
 const pesInicio = () => {
+  taps1.style.display = "block";
+  //   quitarImg.style.display = "block";
+  taps2.style.display = "none";
+  taps4.style.display = "none";
+  taps3.style.display = "none";
+  taps5.style.display = "block";
+  menu.classList.remove("active");
+  sabores.classList.remove("active");
+  ingresar.classList.remove("active");
+  inicio.classList.add("active");
+};
 
+const pesSabores = () => {
+  taps1.style.display = "none";
+  taps2.style.display = "none";
+  taps4.style.display = "none";
+  taps5.style.display = "none";
+  //   quitarImg.style.display = "none";
+  taps3.style.display = "block";
+  inicio.classList.remove("active");
+  menu.classList.remove("active");
+  ingresar.classList.remove("active");
+  sabores.classList.add("active");
+};
 
-taps1.style.display = "block"
-quitarImg.style.display = 'block'
-taps2.style.display = "none"
-taps4.style.display = "none"
-taps3.style.display = "none"
-menu.classList.remove('active')
-sabores.classList.remove('active')
-ingresar.classList.remove('active')
-inicio.classList.add('active')
+const pesIgresar = () => {
+  taps1.style.display = "none";
+  taps2.style.display = "none";
+  taps3.style.display = "none";
+  taps5.style.display = "none";
+  // quitarImg.style.display = 'none'
+  taps4.style.display = "block";
+  inicio.classList.remove("active");
+  menu.classList.remove("active");
+  inicio.classList.remove("active");
+  sabores.classList.remove("active");
+  ingresar.classList.remove("active");
+  ingresar.classList.add("active");
+};
 
-
-}
-
-
-
-const pesSabores = () =>{
-taps1.style.display = "none"
-taps2.style.display = "none"
-taps4.style.display = "none"
-// quitarImg.style.display = 'none'
-taps3.style.display = "block"
-inicio.classList.remove('active')
-menu.classList.remove('active')
-ingresar.classList.remove('active')
-sabores.classList.add('active')
-
-} 
-
-
-const pesIgresar = () =>{
-taps1.style.display = "none"
-taps2.style.display = "none"
-taps3.style.display = "none"
-// quitarImg.style.display = 'none'
-taps4.style.display = "block"
-inicio.classList.remove('active')
-menu.classList.remove('active')
-inicio.classList.remove('active')
-sabores.classList.remove('active')
-ingresar.classList.remove('active')
-ingresar.classList.add('active')
-
-} 
-
-menu.addEventListener('click', pesMenu)
-inicio.addEventListener('click', pesInicio)
-sabores.addEventListener('click', pesSabores)
-ingresar.addEventListener('click', pesIgresar)
-
+menu.addEventListener("click", pesMenu);
+inicio.addEventListener("click", pesInicio);
+sabores.addEventListener("click", pesSabores);
+ingresar.addEventListener("click", pesIgresar);
--- js/obj.js
+++ js/obj.js
@@ -1,136 +1,110 @@
-
-
-const header = document.getElementById('header')
-const inicio = document.getElementById('inicio')
-const menu = document.getElementById('menu')
-const sabores = document.getElementById('sabor')
-const ingresar = document.getElementById('ingresar')
+const header = document.getElementById("header");
+const inicio = document.getElementById("inicio");
+const menu = document.getElementById("menu");
+const sabores = document.getElementById("sabor");
+const ingresar = document.getElementById("ingresar");
 
 // las API del pestana
-const taps1 = document.getElementById('taps1')
-const taps2 = document.getElementById('taps2')
-const taps3 = document.getElementById('taps3')
-const taps4 = document.getElementById('taps4')
- 
-const partHeader = document.querySelector('#foto_header')
+const taps1 = document.getElementById("taps1");
+const taps2 = document.getElementById("taps2");
+const taps3 = document.getElementById("taps3");
+const taps4 = document.getElementById("taps4");
+const taps5 = document.getElementById("taps5");
 
- class MostrarPestana{
- 	constructor(){
-     this.eyelash()
-     this.logNaber()
-     // this.removeNaveWithScroll()
-     
- 	}
- 	
-eyelash(){
- 		
- 			// quitando el color de hover de cada lista
- 			ingresar.classList.remove('active')
- 			sabores.classList.remove('active')
- 			menu.classList.remove('active')
- 			inicio.classList.remove('active')
+const partHeader = document.querySelector("#foto_header");
 
+class MostrarPestana {
+  constructor() {
+    this.eyelash();
+    this.logNaber();
+    // this.removeNaveWithScroll()
+  }
 
- 			// quitar pestana
- 			taps1.style.display = "none"
- 		    taps2.style.display = 'none'
- 		    taps3.style.display = 'none'
- 		    taps4.style.display = 'none'
+  eyelash() {
+    // quitando el color de hover de cada lista
+    ingresar.classList.remove("active");
+    sabores.classList.remove("active");
+    menu.classList.remove("active");
+    inicio.classList.remove("active");
 
- 		    // colocando el evento a la lista
+    // quitar pestana
+    taps1.style.display = "none";
+    taps2.style.display = "none";
+    taps3.style.display = "none";
+    taps4.style.display = "none";
 
- 		    const list =  event.target 
+    // colocando el evento a la lista
 
- 		    // mover el pestana con el evento
+    const list = event.target;
 
- 		    const bodyTaps = list.dataset.body;
-            // agrupar la pesta pa luego mostra
- 		    const viewBody = document.querySelector(bodyTaps)
-            // mostrando el cuerpo de cada pestana
-            // debugger
-            viewBody.style.display = 'block'
-            
-            
-            // mover el color de cada pestana segun este el evento
- 		    list.classList.add('active')
- 		
- 		
- 	}
+    // mover el pestana con el evento
 
-// section donde pone color a la barra y quita la foto header
-logNaber(){
+    const bodyTaps = list.dataset.body;
+    // agrupar la pesta pa luego mostra
+    const viewBody = document.querySelector(bodyTaps);
+    // mostrando el cuerpo de cada pestana
+    // debugger
+    viewBody.style.display = "block";
 
-    
-	
-    window.addEventListener('click', (e) =>{
-    //logrado no es el mejor reto cambiar el codigo luego
-    // mostrar una barra al darle click segun la pestana
+    // mover el color de cada pestana segun este el evento
+    list.classList.add("active");
+  }
 
-    	if( e.target === menu || e.target === sabores || e.target === ingresar){
-    	  partHeader.classList.add('nave')
-           console.log(partHeader)
-    	}else if(e.target === inicio){
-    
-    	partHeader.classList.remove('nave')
-       
-    	}
-      })  
-	} 
-    
- }
+  // section donde pone color a la barra y quita la foto header
+  logNaber() {
+    window.addEventListener("click", (e) => {
+      if (e.target === menu || e.target === sabores || e.target === ingresar) {
+        taps5.style.display = "none";
+      } else if (e.target === inicio) {
+        taps5.style.display = "block";
+      }
+    });
+  }
+}
 
-
-
-
-header.addEventListener('click', () =>{
-	let mostrarPestana = new MostrarPestana()
-	
-})
-
+header.addEventListener("click", () => {
+  let mostrarPestana = new MostrarPestana();
+});
 
 // section de la pestana de menu y la parra de navegacion
 
 class EyelashTwo {
-    //quitar la lista segun la pestana
-    constructor(){
-        this.removeNav()
-    }
+  //quitar la lista segun la pestana
+  constructor() {
+    this.removeNav();
+  }
 
+  removeNav() {
+    // Quitar la nav de la pestana inicio
 
-    removeNav(){
-     // Quitar la nav de la pestana inicio
-     
     let Localizar = window.pageYOffset;
-    window.onscroll = ()=>{
-         let desplazar = window.pageYOffset
-         if(Localizar < desplazar){
-            header.style.display = 'none'
-        }else{
-           header.style.display = 'block' 
-        }
-    }
+    window.onscroll = () => {
+      let desplazar = window.pageYOffset;
+      if (Localizar < desplazar) {
+        header.style.display = "none";
+      } else {
+        header.style.display = "block";
+      }
+    };
     //Mantener el nav en las pestanas menu, sabor, ingresar
 
-    window.addEventListener('click', e =>{
-          window.onscroll = ()=>{
-        let desplazar = window.pageYOffset
-     
-        if(Localizar < desplazar && e.target != menu && e.target != sabor && e.target != ingresar ){
+    window.addEventListener("click", (e) => {
+      window.onscroll = () => {
+        let desplazar = window.pageYOffset;
 
-        header.style.display = 'none'
-
+        if (
+          Localizar < desplazar &&
+          e.target != menu &&
+          e.target != sabor &&
+          e.target != ingresar
+        ) {
+          header.style.display = "none";
         } else {
-         header.style.display = 'block'
-      
-       
+          header.style.display = "block";
         }
-             
-     }
-    })
-   
- }
-
+      };
+    });
+  }
 }
 
-const eyelashTwo = new EyelashTwo()
-
+const eyelashTwo = new EyelashTwo();
